c943134aa92315a326ded1797d5eb1480cb80431
Mansor Almossa <malmossa@gmail.com>
2022-01-04 13:20:24 -0800

    Completed

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
-const correctAnswers = ["B", "B", "A", "B"];
-
 const form = document.querySelector(".quiz-form");
+const correctAnswers = ["B", "B", "A", "B"];
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  alert("Hello");
+
+  let userScore = 0;
+
+  const userAnswers = [
+    form.q1.value,
+    form.q2.value,
+    form.q3.value,
+    form.q4.value,
+  ];
+
+  // Check answers
+  userAnswers.forEach((answer, index) => {
+    if (answer === correctAnswers[index]) {
+      userScore += 25;
+    }
+  });
+
+  console.log(userScore);
 });
